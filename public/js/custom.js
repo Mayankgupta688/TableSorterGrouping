b01c47fe340b56b6344e252033262a903fb85066
Mayank Gupta (ARC) <mayank.gupta@iontrading.com>
2018-12-28 12:17:08 +0530

    Changes for User Grouping Changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,52 @@
+var tableBody = (`
+        <tbody>
+            <tr><td>Good Toys</td><td>USA</td><td>France</td><td></td><td>USA</td></tr>
+            <tr><td>Cycle Clearance</td><td>FRANCE</td><td>Canade</td><td></td><td>UK</td></tr>
+            <tr><td>Cycle Initial Bike Company</td><td>USA</td><td>USA</td><td></td><td>UK</td></tr>
+            <tr><td>Sports Store</td><td>USA</td><td>Canada</td><td></td><td>INDIA</td></tr>
+            <tr><td>Locks Company</td><td>UK</td><td>USA</td><td></td><td>USA</td></tr>
+            <tr><td>Famous Bike Shop</td><td>USA</td><td>USA</td><td></td><td>USA</td></tr>
+            <tr><td>Good Toys</td><td>USA</td><td>USA</td><td></td><td>USA</td></tr>
+            <tr><td>Cycle Clearance</td><td>FRANCE</td><td>USA</td><td></td><td>UK</td></tr>
+            <tr><td>Cycle Initial Bike Company</td><td>USA</td><td>USA</td><td></td><td>UK</td></tr>
+            <tr><td>Sports Store</td><td>USA</td><td>USA</td><td></td><td>INDIA</td></tr>
+            <tr><td>Locks Company</td><td>UK</td><td>USA</td><td></td><td>USA</td></tr>
+            <tr><td>Famous Bike Shop</td><td>USA</td><td>USA</td><td></td><td>USA</td></tr>
+        </tbody>`);
+
+var tableHead = (`
+    <thead>
+        <tr>
+            <th></th>
+            <th class="group-word">Priority</th>
+            <th class="group-word">Status</th>
+            <th class="group-word">Price</th>
+            <th class="group-word">Order Data</th>
+        </tr>
+    </thead>`);
+
 function triggerTableSorter() {
     $("table").tablesorter({
         theme : 'blue',
-        sortList : [[1,0]],
         widgets: ['group', 'filter']
     });
 }
 
-function createTable(sortColumnCount) {
+function createTable() {
+
+    var sortColumnCount = $(".groupingValue").val();
+
     var tableSorter = $("<table></table");
 
-    tableSorter.addClass("tablesorter").attr("data-sortlist", "[[" + sortColumnCount, ", 1]]");
+    tableSorter.addClass("tablesorter").attr("data-sortlist", "[[" + sortColumnCount +", 1]]")
+        .append(tableBody).append(tableHead);
+
+    $(".createTable").append(tableSorter);
+
+    triggerTableSorter();
+}
+
+function updateTableSorter(sortColumnCount) {
+    $(".createTable").find("table").remove();
+    createTable();
 }
